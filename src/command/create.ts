ea32bdc2e2cc40b3b55ae49e35d6dd448d174a1f
Alex Hicks <alex@alexhicks.net>
2019-11-21 10:10:20 -0500

    Minor fixes

--- src/command/create.ts
+++ src/command/create.ts
@@ -5,6 +5,8 @@ import * as fs from "fs-extra";
 import * as recursiveCopy from "recursive-copy";
 import { LogicError } from "../util/LogicError";
 
+const exec = promisify(childProcess.exec);
+
 export const create = async([name]: string[]) => {
   if (!name) {
     throw new LogicError("Usage: service-cli create <name>");
@@ -15,6 +17,8 @@ export const create = async([name]: string[]) => {
     throw new LogicError(`Target directory ${targetDir} already exists!`);
   }
   await fs.mkdirp(targetDir);
+  console.log("Initializing Git repository...");
+  await exec("git init", { cwd: targetDir });
   console.log("Copying template...");
   const results = await recursiveCopy(templateDir, targetDir, {
     dot: true
@@ -22,12 +26,14 @@ export const create = async([name]: string[]) => {
   for (const result of results) {
     console.log(`\t${result.src} -> ${result.dest}`);
   }
+  console.log("Fixing .gitignore...");
+  await fs.move(path.resolve(targetDir, ".npmignore"), path.resolve(targetDir, ".gitignore"));
   console.log("Modifying metadata files...");
   const packageFilename = path.resolve(targetDir, "package.json");
   const packageInfo = await fs.readJSON(packageFilename);
   packageInfo.name = name;
   await fs.writeFile(packageFilename, JSON.stringify(packageInfo, undefined, 2));
   console.log("Installing NPM modules...");
-  await promisify(childProcess.exec)("npm i", { cwd: targetDir });
+  await exec("npm i", { cwd: targetDir });
   console.log("Finished!");
 };
